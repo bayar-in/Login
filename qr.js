71f076fc1b54c17522b50d1aefb68413680b2e4a
Ukasyah Abdulloh Azzam <114288008+Zamy03@users.noreply.github.com>
2024-11-08 08:26:46 +0700

    fix qr pt3

--- qr.js
+++ qr.js
@@ -2,7 +2,7 @@ import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/j
 import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/config.js";
    
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
-wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5ODcxNDA3MDA/dGV4dD13aDR0NWF1dGgw";
+wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg1MTU3OTc5NzU5P3RleHQ9d2g0dDVhdXRoMA==";
 wauthparam.tokencookiehourslifetime=18;
 wauthparam.redirect ="/auth"
 deleteCookie(wauthparam.tokencookiename);
